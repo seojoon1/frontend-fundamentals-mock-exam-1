d3670fd334a0052101867cfeba6ae9cfc1fb82fd
comppy <kim1122aass@gmail.com>
2025-11-21 21:24:17 +0900

    feat: productlist

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -71,21 +71,25 @@ export function SavingsCalculatorPage() {
         </Tab.Item>
       </Tab>
 
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'기본 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={'연 이자율: 3.2%'}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={'100,000원 ~ 500,000원 | 12개월'}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        right={<Assets.Icon name="icon-check-circle-green" />}
-        onClick={() => {}}
-      />
+      {products
+        .filter((product) => product.minMonthlyAmount <= monthlyAmount! && product.maxMonthlyAmount >= monthlyAmount! && product.availableTerms === term)
+        .map((product) => (
+          <ListRow
+            contents={
+              <ListRow.Texts
+                type="3RowTypeA"
+                top={product.name}
+                topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+                middle={`연 이자율: ${product.annualRate}%`}
+                middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+                bottom={`${product.minMonthlyAmount}원~${product.maxMonthlyAmount}원 | ${product.availableTerms}개월`}
+                bottomProps={{ fontSize: 13, color: colors.grey600 }}
+              />
+            }
+            right={<Assets.Icon name="icon-check-circle-green" />}
+            onClick={() => {}}
+        />
+        ))}
       {/* <ListRow
         contents={
           <ListRow.Texts
